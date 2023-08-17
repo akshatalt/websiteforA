fbf38e87923c3e929e5782362545f06ac2fab17f
akshatalt <140099071+akshatalt@users.noreply.github.com>
2023-08-17 21:06:27 +0530

    Add files via upload

---
+++ clickmescript.js
@@ -0,0 +1,36 @@
+var dv = document.getElementById("content");
+var val = 0;
+
+function timer(){
+	var start = new Date(2022, 11, 31, 0, 54);
+	var t = new Date() - start;
+	var d = Math.floor(t / 1000 / 60 / 60 / 24);
+	var h = Math.floor(t / 1000 / 60 / 60 % 24);
+	if(h < 10){
+		h = "0" + h; 
+	}
+	var m = Math.floor(t / 1000 / 60 % 60);
+	if(m < 10){
+		m = "0" + m;
+	}
+	var s = Math.floor(t / 1000 % 60);
+	if(s < 10){
+		s = "0" + s;
+	}
+	document.getElementById("d").innerHTML = d;
+	document.getElementById("h").innerHTML = h;
+	document.getElementById("m").innerHTML = m;
+	document.getElementById("s").innerHTML = s;
+}
+
+var fadeInterval;
+var fadeinInterval;
+
+timer();
+setInterval(timer, 1000);
+fadeInterval = setInterval(function(){
+	if(ok == 2){
+		clearInterval(fadeInterval);
+		fadeinInterval = setInterval(fadein, 50);
+	}
+}, 50)
